21f0b8cc07143dbe47cbe45c8d1873893b9f0371
r3pt1s <leonistmegacool2@gmail.com>
2024-12-11 18:54:16 +0100

    first classes

---
+++ src/r3pt1s/betternpc/command/EntityMainCommand.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace r3pt1s\betternpc\command;
+
+//TODO: Commandos library
+final class EntityMainCommand {
+
+}
---
+++ src/r3pt1s/betternpc/listener/EventListener.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace r3pt1s\betternpc\listener;
+
+use pocketmine\event\Listener;
+
+final class EventListener implements Listener {
+
+}
