79bfa39a04cdef9cb6eae7cf039669a12942516d
r3pt1s <leonistmegacool2@gmail.com>
2024-12-13 20:24:06 +0100

    .forgot some methods

--- src/r3pt1s/betternpc/entity/impl/BetterHuman.php
+++ src/r3pt1s/betternpc/entity/impl/BetterHuman.php
@@ -44,6 +44,11 @@ final class BetterHuman extends Human implements BetterEntity {
         $this->entityData->getSettings()->setNameTagAlwaysVisible($value);
     }
 
+    protected function entityBaseTick(int $tickDiff = 1): bool {
+        $this->emoteTick();
+        return parent::entityBaseTick($tickDiff);
+    }
+
     public function saveNBT(): CompoundTag {
         $nbt = parent::saveNBT();
         $nbt->setTag("entityData", $this->entityData->toNbt());
--- src/r3pt1s/betternpc/entity/impl/BetterZombie.php
+++ src/r3pt1s/betternpc/entity/impl/BetterZombie.php
@@ -39,6 +39,16 @@ final class BetterZombie extends Zombie implements BetterEntity {
         $this->entityData->setScale($value);
     }
 
+    public function setNameTagAlwaysVisible(bool $value = true): void {
+        parent::setNameTagAlwaysVisible($value);
+        $this->entityData->getSettings()->setNameTagAlwaysVisible($value);
+    }
+
+    protected function entityBaseTick(int $tickDiff = 1): bool {
+        $this->emoteTick();
+        return parent::entityBaseTick($tickDiff);
+    }
+
     public function saveNBT(): CompoundTag {
         $nbt = parent::saveNBT();
         $nbt->setTag("entityData", $this->entityData->toNbt());
